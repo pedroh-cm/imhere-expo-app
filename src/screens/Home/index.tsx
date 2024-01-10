e6c6cd10c490b690f803ee4158178a5973ef7aef
Pedro Henrique Carlos Morinelli <pedro.morinelli@cobasi.com.br>
2024-01-09 22:48:02 -0300

    created component form with styles

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View } from "react-native"
+import { Text, View, TextInput, TouchableOpacity } from "react-native"
 
 import { styles } from "./styles"
 
-export default function Home() {
+export function Home() {
+
+  function handleParticipantAdd() {
+    console.log('Adicionar participante')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -11,6 +16,18 @@ export default function Home() {
       <Text style={styles.eventDate}>
         Sexta, 9 de Janeiro de 2024
       </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
       padding: 24,
     },
     eventName: {
-      color: "#FDFCFE",
+      color: "#FFF",
       fontWeight: "bold",
       fontSize: 24,
       marginTop: 48,
@@ -15,5 +15,33 @@ export const styles = StyleSheet.create({
     eventDate: {
       color: "#6B6B6B",
       fontSize: 16,
+    },
+    input: {
+      height: 56,
+      backgroundColor: "#1F1E25",
+      borderRadius: 5,
+      color: "#FFF",
+      padding: 16,
+      fontSize: 16,
+      flex: 1,
+      marginRight: 12
+    },
+    buttonText: {
+      color: "#FFF",
+      fontSize: 24,
+    },
+    button: {
+      width: 56,
+      height: 56,
+      borderRadius: 5,
+      backgroundColor: "#31CF67",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    form: {
+      width: "100%",
+      flexDirection: "row",
+      marginTop: 36,
+      marginBottom: 42,
     }
   });
