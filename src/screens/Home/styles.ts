5bdd6fcd8e468ed020c1f39761af7f3c9a0f87c1
Pedro Henrique Carlos Morinelli <pedro.morinelli@cobasi.com.br>
2024-01-09 22:12:44 -0300

    created folder src for organization project

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#131016",
+      padding: 24,
+    },
+    eventName: {
+      color: "#FDFCFE",
+      fontWeight: "bold",
+      fontSize: 24,
+      marginTop: 48,
+    },
+    eventDate: {
+      color: "#6B6B6B",
+      fontSize: 16,
+    }
+  });
